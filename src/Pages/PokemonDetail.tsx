c19a05b29d6f1bf44f02da0c3cf3dd27ba260a7a
Krisp1k <czraketa@gmail.com>
2024-03-18 00:07:57 +0100

    setup

---
+++ src/Pages/PokemonDetail.tsx
@@ -0,0 +1,8 @@
+
+export default function PokemonDetail() {
+    return (
+        <div>
+            <h1>Pokemon Detail</h1>
+        </div>
+    )
+}
---
+++ src/Pages/PokemonList.tsx
@@ -0,0 +1,7 @@
+export default function PokemonList() {
+    return (
+        <div>
+            <h1>Pokemon List</h1>
+        </div>
+    )
+}
